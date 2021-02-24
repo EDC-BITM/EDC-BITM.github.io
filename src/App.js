a9808bae17190b0faab8b45aeb72425519b580d3
Aman Srivastava <amansrivastava905@gmail.com>
2021-02-24 13:49:43 +0530

    gh pages added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import React from 'react'
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Navbar />
         <Switch>
           <Route path="/" exact component={LandingPage} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
