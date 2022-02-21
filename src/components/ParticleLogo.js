246847b471e1d9a97bc5e091c8651057f5bf84a4
saranggupta11 <sarang.gupta0811@gmail.com>
2022-02-21 12:13:36 +0530

    gh1

--- src/components/ParticleLogo.js
+++ src/components/ParticleLogo.js
@@ -1,149 +1,166 @@
 import React from "react";
 import styled from "styled-components";
 import Particles from "react-tsparticles";
-import EdcLogo from "../assets/vector1.svg";
 const ParticleLogo = () => {
   const particlesInit = (main) => {
     console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
-
   const particlesLoaded = (container) => {
     console.log(container);
   };
-
   return (
     <>
-      <Particles
-        id="tsparticles"
-        width="200"
-        height="200"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: "#000000",
-            },
-            position: "50% 50%",
-            repeat: "no-repeat",
-            size: "cover",
-          },
-          detectRetina: false,
-          interactivity: {
-            events: {
-              onClick: {
-                mode: "push",
+      <Container>
+        <HeadingWrapper>
+          <Sponsor>Angle<SponsorSpan>One</SponsorSpan></Sponsor>
+        <Heading>E-SUMMIT'22</Heading>
+        </HeadingWrapper>
+        <Logo>
+          <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+              width: {
+                value: 1000,
               },
-              onDiv: {
-                selectors: "#repulse-div",
-                mode: "repulse",
-              },
-              onHover: {
-                enable: true,
-                mode: "bubble",
-              },
-            },
-            modes: {
-              bubble: {
-                distance: 40,
-                duration: 2,
-                opacity: 8,
-                size: 6,
-              },
-              grab: {
-                distance: 400,
-              },
-              slow: {
-                factor: 1,
-                radius: 0,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#FFA500",
-            },
-            links: {
-              color: {
-                value: "#FFA500",
-              },
-              distance: 30,
-              enable: true,
-              opacity: 0.4,
-            },
-            move: {
-              attract: {
-                rotate: {
-                  x: 600,
-                  y: 1200,
+              detectRetina: false,
+              interactivity: {
+                events: {
+                  onClick: {
+                    mode: "push",
+                  },
+                  onDiv: {
+                    selectors: "#repulse-div",
+                    mode: "repulse",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "bubble",
+                  },
+                },
+                modes: {
+                  bubble: {
+                    distance: 40,
+                    duration: 2,
+                    opacity: 8,
+                    size: 6,
+                  },
+                  grab: {
+                    distance: 400,
+                  },
+                  slow: {
+                    factor: 5,
+                    radius: 0,
+                  },
                 },
               },
-              enable: true,
-              outModes: {
-                default: "bounce",
-                bottom: "bounce",
-                left: "bounce",
-                right: "bounce",
-                top: "bounce",
-              },
-              speed: 0.1,
-            },
-            number: {
-              density: {
-                area: 2000,
-              },
-              value: 100,
-            },
-            opacity: {
-              value: {
-                min: 0.5,
-                max: 0.8,
-              },
-              animation: {
-                enable: true,
-                minimumValue: 0.05,
-              },
-            },
-            size: {
-              random: {
-                enable: true,
-              },
-              value: 1,
-              animation: {
-                speed: 40,
-                minimumValue: 0.1,
-              },
-            },
-          },
-          polygon: {
-            draw: {
-              enable: true,
-              stroke: {
+              particles: {
                 color: {
-                  value: "rgba(255,255,255,0.2)",
+                  value: "#FFA500",
                 },
-                width: 0.5,
-                opacity: 0.2,
+                links: {
+                  color: {
+                    value: "#FFA500",
+                  },
+                  distance: 30,
+                  enable: true,
+                  opacity: 0.4,
+                },
+                move: {
+                  attract: {
+                    rotate: {
+                      x: 600,
+                      y: 1200,
+                    },
+                  },
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
+                    bottom: "bounce",
+                    left: "bounce",
+                    right: "bounce",
+                    top: "bounce",
+                  },
+                  speed: 0.1,
+                },
+                number: {
+                  density: {
+                    area: 2000,
+                  },                            
+                  value: 100,
+                },
+                opacity: {
+                  value: {
+                    min: 0.5,
+                    max: 0.8,
+                  },
+                  animation: {
+                    enable: true,
+                    minimumValue: 0.05,
+                  },
+                },
+                size: {
+                  value: 1,
+                  animation: {
+                    speed: 0,
+                    minimumValue: 0.1,
+                  },
+                },
               },
-            },
-            enable: true,
-            inline: {
-              arrangement: "equidistant",
-            },
-            move: {
-              radius: 10,
-              type: "path",
-            },
-            scale: 0.5,
-            type: "inline",
-            url: "https://res.cloudinary.com/hevently-sarang/image/upload/v1645087891/vector1_br63e2.svg",
-          },
-        }}
-      />
+              polygon: {
+                draw: {
+                  enable: true,
+                  stroke: {
+                    color: {
+                      value: "rgba(255,255,255,0.2)",
+                    },
+                    width: 0.5,
+                    opacity: 0.2,
+                  },
+                },
+                enable: true,
+                inline: {
+                  arrangement: "equidistant",
+                },
+                move: {
+                  radius: 10,
+                  type: "path",
+                },
+                scale: 0.5,
+                type: "inline",
+                url: "https://res.cloudinary.com/hevently-sarang/image/upload/v1645087891/vector1_br63e2.svg",
+              },
+            }}
+          />
+        </Logo>
+      </Container>
     </>
   );
 };
 
 export default ParticleLogo;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Heading = styled.h1`
+  color: #fff;
+  font-size: 4rem;
+`;
+
+const Logo = styled.div`
+`;
+
+const HeadingWrapper = styled.div`
+`;
+
+const Sponsor = styled.h4`
+  color: #fff;
+`;
+
+const SponsorSpan = styled.span`
+  color: #FFA500;
+`;
